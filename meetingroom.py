93023b8186c6dedcbbb9761549b2adabb3402b44
sarba-jit <sarbajit.mukherjee@aggiemail.usu.edu>
2016-02-26 17:12:56 -0700

    added comments

--- meetingroom.py
+++ meetingroom.py
@@ -7,11 +7,17 @@ def minMeetingRooms(intervals):
         print i
         if heap and i[0] >= heap[0]: 
             # means two intervals can use the same room even if their end and start time might match
-            heapq.heapreplace(heap, i[1]) #pops and returns the smallest item from the list, which basically means that it pops the smallest end time,i.e., that particular meeting has ended and that the room can be allocated to the new meeting. It then adds the end time of the new meeting.
+            heapq.heapreplace(heap, i[1]) 
+		#pops and returns the smallest item from the list, which basically
+		#means that it pops the smallest end time,i.e., that particular meeting has 
+		#ended and that the room can be allocated to the new meeting. 
+		##It then adds the end time of the new meeting.
             #print i[1]
         else:
             # a new room is allocated
-            heapq.heappush(heap, i[1]) #if a meeting is already going, it assigns a new room to the meeting and thus adds the end time of that meeting to the heap.
+            heapq.heappush(heap, i[1]) 
+		#if a meeting is already going, it assigns a new room to the meeting 
+		#and thus adds the end time of that meeting to the heap.
             print i[1]
         print heap
         print '\n'
